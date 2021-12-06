32a4bee15a29ad7d9eb84f744e9de74230d73769
Vitor Dutra <jo.vitor.d@gmail.com>
2021-12-05 22:16:40 -0300

    Create My First Widget -> MaterialApp()

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
