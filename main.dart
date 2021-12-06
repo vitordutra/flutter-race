57006416c95a5d39794f84149ddeebc54afb216e
Vitor Dutra <jo.vitor.d@gmail.com>
2021-12-05 21:50:56 -0300

    Change User class to named arguments

--- main.dart
+++ main.dart
@@ -1,10 +1,10 @@
 void main() async {
-  final user = User("João Vitor Dutra", "joaovitor@email.com");
+  final user = User(name: "João Vitor Dutra", email: "joaovitor@email.com");
   print(user.email);
 }
 
 class User {
   final String name;
   final String email;
-  User(this.name, this.email);
+  User({required this.name, required this.email});
 }
