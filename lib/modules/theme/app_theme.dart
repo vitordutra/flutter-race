837f914d76c6386ec5c68d4c436fa2beeaf02bf6
Vitor Dutra <jo.vitor.d@gmail.com>
2021-12-08 08:34:34 -0300

    Finish until 41:36

--- lib/modules/theme/app_theme.dart
+++ lib/modules/theme/app_theme.dart
@@ -3,5 +3,8 @@ import 'package:flutter_race/modules/theme/app_text_styles.dart';
 
 class AppTheme {
   final _colors = AppColors();
+  AppColors get colors => _colors;
+
   final _textStyles = AppTextStyles();
+  AppTextStyles get textStyles => _textStyles;
 }
